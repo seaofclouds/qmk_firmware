f9d8e23f8a9ebf34321bedbc898041f80bcd04ad
seaofclouds <info@seaofclouds.com>
2019-04-17 22:41:44 -0700

    add layer indicators, backlit

--- keyboards/dz60/keymaps/seaofclouds_mac_splitspace/keymap.c
+++ keyboards/dz60/keymaps/seaofclouds_mac_splitspace/keymap.c
@@ -1,32 +1,65 @@
 #include QMK_KEYBOARD_H
 
+#define _ANSI 0
+#define _ARROWS 1
+#define _CONFIG 2
+#define _FUNCTION 3
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_60_ansi_splitspace_splitrshift(
+	[_ANSI] = LAYOUT_60_ansi_splitspace_splitrshift(
 		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, TG(3),
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(2), KC_SPC, MO(1), KC_RGUI, KC_NO, KC_RALT, KC_RCTL
+		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, TG(_ARROWS),
+		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_CONFIG), KC_SPC, MO(_FUNCTION), KC_RGUI, KC_NO, KC_RALT, KC_RCTL
 	),
-	[1] = LAYOUT_60_ansi_splitspace_splitrshift(
-		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL,
+	[_ARROWS] = LAYOUT_60_ansi_splitspace_splitrshift(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_NO, KC_DOWN, KC_RIGHT
+	),
+	[_CONFIG] = LAYOUT_60_ansi_splitspace_splitrshift(
+		KC_GRV, KC_F15, KC_F14, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL,
 		KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 	),
-	[2] = LAYOUT_60_ansi_splitspace_splitrshift(
-		KC_TRNS, M(1), M(2), M(3), M(4), M(5), M(6), M(7), M(8), M(9), M(10), M(11), M(12), KC_TRNS, KC_TRNS,
+	[_FUNCTION] = LAYOUT_60_ansi_splitspace_splitrshift(
+		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 	),
-	[3] = LAYOUT_60_ansi_splitspace_splitrshift(
-		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_TRNS,
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(2), KC_SPC, MO(1), KC_LEFT, KC_NO, KC_DOWN, KC_RIGHT
-	),
+\
+};
+
+// Change LED colors depending on the layer.
+uint32_t layer_state_set_user(uint32_t state) {
+	switch (biton32(state)) {
+		case _ANSI:
+			rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+			rgblight_disable();
+			break;
+		case _ARROWS:
+			rgblight_enable();
+			rgblight_setrgb_orange();
+			break;
+		case _CONFIG:
+			rgblight_enable();
+			rgblight_sethsv_noeeprom_red();
+			break;
+		case _FUNCTION:
+			rgblight_enable();
+			rgblight_setrgb_teal();
+			break;
+		default: //  for any other layers, or the default layer
+		rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+		rgblight_disable();
+			break;
+	}
+	return state;
 };
